14a52eb8fae7ceee4931d1824747ff448775d534
imcdo <ian_mcdonald@rocketmail.com>
2018-06-07 12:50:05 -0500

    basic movement added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] Rigidbody playerRB;
+    [SerializeField] float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+	}
+
+    // Player Movement handled here
+    // vert -vertical movement modifier
+    // horz -horizontal movement modifier
+    void movement (float vert, float horz) {
+        Vector3 playerV = playerRB.velocity;
+        playerV.x = horz * movementSpeed;
+        playerV.z = vert * movementSpeed;
+        playerRB.velocity = playerV;
+    }
+}
